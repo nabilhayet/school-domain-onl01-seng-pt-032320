35070087d0321d9c847517fb9b4b5a8f9501b2bc
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-08 03:41:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School 
+  attr_accessor :roster
+  def initialize(name)
+    @name =name 
+    @roster = {}
+  end 
+  def add_student(name,grade)
+    if @roster[grade]
+        @roster[grade] << name
+      else 
+    @roster[grade] = []
+    @roster[grade] << name
+  
+  end 
+end 
+  def grade(grade)
+    @roster[grade]
+  end 
+
+ def sort 
+   @roster.collect do |key,value|
+     @roster[key]=value.sort
+ end 
+ @roster
+end 
+end 
