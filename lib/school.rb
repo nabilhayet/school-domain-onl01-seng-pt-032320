ced651d63885c0c69bf051583b8d6148c677073e
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-08 03:14:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School 
+  def initialize(name)
+    @name =name 
+    @roster = {}
+  end 
+  def add_student(name,grade)
+    @roster["grade"] = []
+    @roster["grade"] << name
+  end 
+  def grade(grade)
+  @roster.map do |key,value|
+  if grade==key
+    @roster[key]
+  end 
+end 
+end 
+
+ def sort 
+   @roster.sort 
+ end 
+end 
